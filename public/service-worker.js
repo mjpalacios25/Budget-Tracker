54a50aa08f04bb4d132d7564c306eebd3be02054
Moses Palacios <mospalacios@gmail.com>
2020-04-01 19:15:05 -0400

    added db.js to the service worker cached files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 console.log("Hello from your service worker!");
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "style.css"];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js", "style.css", "db.js"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
